484ceebb9be0f83e3093d6fccd5e83d02ae6fd88
Madu-f0r0 <duherbert@gmail.com>
2024-03-18 13:39:10 +0100

    Added type annotations to script 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Waits for a random delay and eventually returns it"""
     delay = random.randint(0, 10)
 
