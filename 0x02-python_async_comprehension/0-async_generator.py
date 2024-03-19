f70e4062efcbeccf618facfdd54edf0b708c489f
Madu-f0r0 <duherbert@gmail.com>
2024-03-19 12:21:40 +0100

    Modifed type annotation for script 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields 10 random numbers between 0 and 10, both inclusive"""
     for i in range(10):
         await asyncio.sleep(1)
